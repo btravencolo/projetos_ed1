03de772e2299731f356fe9c94a7a5dda82177d93
travencolo <travencolo@gmail.com>
2020-10-26 22:58:16 -0300

    aula pilha (26-10)

--- TStack/TStack.h
+++ TStack/TStack.h
@@ -13,5 +13,11 @@ TStack *stack_create();
 int stack_free(TStack *st);
 
 int stack_push(TStack *st, struct aluno al);
+int stack_pop(TStack *st);
+
+int stack_top(TStack *st, struct aluno *al);
+
+int stack_empty(TStack *st);
+int stack_size(TStack *st);
 
 #endif
